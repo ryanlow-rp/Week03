cbbdd5ea31c959510c88baa63514011956a43e9e
Ryan Low <ryan_low@rp.edu.sg>
2023-05-03 23:26:24 +0800

    completed Section E

--- app/src/main/java/sg/edu/rp/c346/id22000000/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000000/week03/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etInput;
     ToggleButton tbtn;
 
+    RadioGroup rgGender;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +28,21 @@ public class MainActivity extends AppCompatActivity {
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
         tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
+
+        rgGender.check(R.id.radioButtonGenderMale);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code for the action
                 String stringResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if (checkedRadioId == R.id.radioButtonGenderMale) {
+                    stringResponse = "He says " + stringResponse;
+                } else {
+                    stringResponse = "She says " + stringResponse;
+                }
                 tvDisplay.setText(stringResponse);
             }
         });
@@ -39,11 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Add your code for the action
-                if (tbtn.isChecked() == true) {
-                    etInput.setEnabled(true);
-                } else {
-                    etInput.setEnabled(false);
-                }
+                etInput.setEnabled(tbtn.isChecked());
             }
         });
 
