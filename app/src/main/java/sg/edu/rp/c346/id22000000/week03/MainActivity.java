f04395908eb2a5379963db6af72ed5f7915c7d09
Ryan Low <ryan_low@rp.edu.sg>
2023-05-03 22:45:35 +0800

    completed Section B

--- app/src/main/java/sg/edu/rp/c346/id22000000/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000000/week03/MainActivity.java
@@ -3,13 +3,24 @@ package sg.edu.rp.c346.id22000000.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+
     }
 }
